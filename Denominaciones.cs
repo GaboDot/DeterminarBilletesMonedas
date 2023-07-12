6dbdf2f7aa28c13ad8af75649907c4b23a0737fe
GaboDot <gfloresolea@outlook.com>
2023-07-11 23:09:12 -0600

    Agregar archivos de proyecto.

---
+++ Denominaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeterminarBilletesMonedas
+{
+    public class Denominaciones
+    {
+        List<double> denominaciones = new List<double>();
+
+        public List<double> buscarDenominaciones(double cantidad, double[] arregloBuscar, out double restoCantidad)
+        {
+            for (int i = arregloBuscar.Length - 1; i >= 0; i--)
+            {
+                while(cantidad >= arregloBuscar[i])
+                {
+                    cantidad -= arregloBuscar[i];
+                    denominaciones.Add(arregloBuscar[i]);
+                }
+            }
+            restoCantidad = cantidad;
+            return denominaciones;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using DeterminarBilletesMonedas;
+
+Console.WriteLine("Introduce cantidad: ");
+double cantidad = double.Parse(Console.ReadLine());
+double nuevaCantidad = 0;
+double[] billetes = new double[] { 20, 50, 100, 200, 500, 1000 };
+double[] monedas = new double[] { 1, 2, 5, 10 };
+List<double> denominaciones = new List<double>();
+Denominaciones busqueda = new Denominaciones();
+
+if (cantidad >= billetes[0])
+{
+    denominaciones = busqueda.buscarDenominaciones(cantidad, billetes, out cantidad);
+    foreach (var number in denominaciones.GroupBy(x => x))
+        Console.WriteLine($"Billete ${number.Key} : {number.Count()}");
+    denominaciones.Clear();
+}
+if(cantidad > 0 )
+{
+    denominaciones = busqueda.buscarDenominaciones(cantidad, monedas, out cantidad);
+    foreach (var number in denominaciones.GroupBy(x => x))
+        Console.WriteLine($"Moneda \t${number.Key} : {number.Count()}");
+}
+Console.ReadKey();
